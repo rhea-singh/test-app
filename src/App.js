203fff68baee28b2c421538e02b931d05a08ec97
rhea-singh <rheasingh2715@gmail.com>
2024-01-05 01:34:32 +0100

    Added initial skeleton for agenda app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import AgendaSection from './modules/agenda-section/AgendaSection';
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <AgendaSection />
     </div>
   );
 }
